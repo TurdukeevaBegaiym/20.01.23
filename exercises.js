b41ebcda995a710743a2fb2723f611dd313b093b
begaiyma <turdukeeva064@gmail.com>
2023-01-20 21:29:23 +0600

    exersis-2

--- exercises.js
+++ exercises.js
@@ -1,3 +1,4 @@
+// У Пети 344 сомов, хватит ли это на мороженное,которое стоит 20сом.  
 let som = 344;
 let icecrim = 20;
 if (som >= icecrim) {
@@ -7,29 +8,34 @@ else {
   console.log("no");
 }
 
+// Проверить является ли  23 четным числом.
 let number = 23;
 if (number % 2 === 0) {
-  console.log("Yes")
+  console.log(number + "is even");
 }
 else {
-  console.log("no")
+  console.log(number + "is odd");
 }
+
+// Является ли вода в жидком состоянии при температуре 101 градусов цельсия.
 let watertemperature = 101;
 if(watertemperature > 0 && watertemperature < 100 ) {
-  console.log("Yes")
+  console.log("water is liguid");
 }
 else{
-  console.log("No")
+  console.log("water is not liguid");
 }
 
+// Проверить является 2000 високосным годом.
 let year = 2000;
-if (2000 / 4) {
-  console.log("Yes")
+if (year % 4 === 0) {
+  console.log("Is leap year");
 }
 else {
-  console.log("No")
+  console.log("Is not a leap year");
 }
 
+// У Васи средня балл по всем урока 4. вывести кем он является(Отличник, Ударник,Троешник,Двоешник,Отчислен)
 let ball = 0;
 
 if (ball === 5) {
@@ -48,5 +54,12 @@ else if(ball === 2) {
 else if(ball === 0) {
   console.log("Отчислен")
 }
-let month = feburary;
-
+ 
+//Сколько дней в Феврале(month)?
+let month = "february";
+if (month === "february") {
+  console.log("29 days");
+}
+else if (month === "April" || month ==="June" || month === "september" || month === "November") {
+  console.log("31 days");
+}
